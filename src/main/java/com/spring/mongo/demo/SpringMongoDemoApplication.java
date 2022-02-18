266718c5a58ab02ba35a78927b7d0167834d26b9
Zhiqiagdaniel.Xu <Zhiqiagdaniel.Xu@ontario.ca>
2022-02-18 15:14:19 -0500

    Added global timezone setup

--- src/main/java/com/spring/mongo/demo/SpringMongoDemoApplication.java
+++ src/main/java/com/spring/mongo/demo/SpringMongoDemoApplication.java
@@ -1,15 +1,17 @@
 package com.spring.mongo.demo;
 
 import com.spring.mongo.demo.service.AggregationService;
+import com.spring.mongo.demo.util.DateUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @Slf4j
 @SpringBootApplication
@@ -29,14 +31,10 @@ public class SpringMongoDemoApplication {
         SpringApplication.run(SpringMongoDemoApplication.class, args);
     }
 
-    @Bean
-    CommandLineRunner init() {
-
-        return new CommandLineRunner() {
-            public void run(String... args) throws Exception {
-                log.info("=============== Get Courses/Students By Lookup===============");
-//                aggregationService.findCoursesWithStudents();
-            }
-        };
+    // Regardless the app server and DB server time zone settings, the application
+    // always use UTC as the default time zone
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone(DateUtils.DEFAULT_ZONE_ID));
     }
 }
--- src/main/java/com/spring/mongo/demo/util/DateUtils.java
+++ src/main/java/com/spring/mongo/demo/util/DateUtils.java
@@ -2,11 +2,13 @@ package com.spring.mongo.demo.util;
 
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 public class DateUtils {
 
     private final static String DEFAULT_DATE_PATTERN = "yyyyMMdd_HHmm";
+    public static final ZoneId DEFAULT_ZONE_ID = ZoneId.of("UTC");
 
     public static String getDateAsString(LocalDateTime dateTime) {
         DateTimeFormatter formatter  = DateTimeFormatter.ofPattern(DEFAULT_DATE_PATTERN);
